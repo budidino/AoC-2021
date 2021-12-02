af5eb648934177edcef160a9182bbd9e45e3ced0
Dino Budimilic <budidino@gmail.com>
2021-12-02 18:29:41 +0100

    day2

--- src/day2/solution.kt
+++ src/day2/solution.kt
@@ -1,13 +1,43 @@
 fun main() {
-    fun solve(input: List<Int>, size: Int): Int {
-        return input.windowed(size) { it.sum() }.zipWithNext().count { it.second > it.first }
+    fun part1(input: List<String>): Int {
+        var x = 0
+        var y = 0
+        for (row in input) {
+            val split = row.split(" ")
+            val direction = split[0]
+            val distance = split[1].toInt()
+            if (direction == "forward") {
+                x += distance
+            } else {
+                y += if (direction == "up") -distance else distance
+            }
+        }
+        return x * y
     }
 
-    val testInput = readInputInt("day2/test")
-    check(solve(testInput, 1) == 7)
-    check(solve(testInput, 3) == 5)
+    fun part2(input: List<String>): Int {
+        var x = 0
+        var y = 0
+        var aim = 0
+        for (row in input) {
+            val split = row.split(" ")
+            val direction = split[0]
+            val distance = split[1].toInt()
+            if (direction == "forward") {
+                x += distance
+                y += aim * distance
+            } else {
+                aim += if (direction == "up") -distance else distance
+            }
+        }
+        return x * y
+    }
 
-    val input = readInputInt("day2/input")
-    println("PART 1: " + solve(input, 1))
-    println("PART 2: " + solve(input, 3))
+    val testInput = readInput("day2/test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
+
+    val input = readInput("day2/input")
+    println("PART 1: " + part1(input))
+    println("PART 2: " + part2(input))
 }
